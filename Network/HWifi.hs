438118116a5bb531721d66ceac624f2577ec99f5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-12 18:47:14 +0100

    Use [] for empty string

--- Network/HWifi.hs
+++ Network/HWifi.hs
@@ -77,7 +77,7 @@ filterKnownWifi autoConnectWifis = filter $ (== True) . fst . first (`elem` auto
 
 -- | Given a wifi, execute the command to connect to a wifi
 commandConnectToWifi :: [String] -> String
-commandConnectToWifi []     = ""
+commandConnectToWifi []     = []
 commandConnectToWifi [wifi] = "nmcli con up id " ++ wifi
 
 -- | Elect wifi according to signal's power (the most powerful is elected)
