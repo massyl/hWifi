4f3e1b7da73ad29c9c0cfc00f5a2a3e61095eb9a
Massyl Nait-Mouloud <massil.nait@gmail.com>
2014-03-15 20:02:13 +0100

    Some code refactoring

--- Network/HWifi.hs
+++ Network/HWifi.hs
@@ -18,16 +18,20 @@ module Network.HWifi where
 -----------------------------------------------------------------------------
 
 import System.Process
-import Data.Function (on)
 import Data.Functor
-import Data.List (sortBy, intersect, delete, isPrefixOf)
+import Data.List (intersect, delete, isPrefixOf, sort)
 import Control.Monad.Writer hiding(mapM_)
-import Data.Foldable hiding (mapM_)
 import Prelude hiding(elem)
 import Control.Monad.Error
-import Control.Arrow (first, (***))
-type Wifi w a = WriterT w IO a
-data Command = Scan String | ListKnown String | Connect String
+import Control.Arrow ((***), second)
+
+type WifiMonad w a = WriterT w IO a
+
+type Wifi = (String, String)
+
+type SSID = String
+
+data Command = Scan{ scan :: String} | Connect {connect :: String -> String}
 
 {--
   Command to scan the current wifi
@@ -37,11 +41,11 @@ scanCmd = Scan "nmcli --terse --fields ssid,signal dev wifi"
 
 -- | Command to list the wifi the computer can currently auto connect to
 knownCmd :: Command
-knownCmd = ListKnown "nmcli --terse --fields name con list"
+knownCmd = Scan "nmcli --terse --fields name con list"
 
 -- | Given a wifi, execute the command to connect to a wifi (need super power :)
-connectCmd :: String -> String
-connectCmd = ("sudo nmcli con up id " ++)
+conCmd :: Command
+conCmd = Connect ("sudo nmcli con up id " ++)
 
 -- | Run a command and displays the output in list of strings
 run :: String -> IO [String]
@@ -49,42 +53,36 @@ run command = readProcess comm args [] >>= return . lines
   where (comm:args) = words command
 
 -- | Utility function to trim the ' in a string
-cleanString :: String -> String
-cleanString s = if (elem '\'' s) then tail . init $ s
-                else s
+clean :: Char -> String -> String
+clean c cs = if isPrefixOf [c] cs then sanitize cs else cs
+  where sanitize = delChar . reverse . delChar . reverse
+        delChar  = delete c
 
 -- | Slice a string "'wifi':signal" in a tuple ("wifi", "signal")
-parseWifi :: String -> (String, String)
-parseWifi s = (cleanString ssid, tail signal)
-  where (ssid, signal) = break (== ':') s
+parse :: String -> Wifi
+parse s = wifiDetail
+  where wifiDetail = clean ('\'') *** tail $ break (== ':') s
 
--- | Given a list of signals, return the list of couple (wifi, signal)
-parseWifis :: [String] -> [(String, String)]
-parseWifis = map parseWifi
+available:: Command -> WifiMonad [String] [SSID]
+available (Scan cmd) = runWithLog allWifis logAll
+  where allWifis = (map (fst . second sort) . map parse) <$> run cmd
+        logAll = logMsg ("Scanned wifi: \n") ("- "++)
 
--- | Scan the proximity wifi and return a list of (ssid, signal).
-scanWifis:: Command -> Wifi [String] [String]
-scanWifis (Scan cmd) = runWithLog (map fst . reverse . sortBy (compare `on` snd) . map parseWifi <$> run cmd) logAll
-
--- execCommand :: (Foldable f) => Command -> Wifi [String] (f a)
--- execCommand (Scan cmd) = runWithLog (foldMap parseWifi <$> run cmd) logAll
 
 -- | List the current wifi the computer can connect to
-getKnownWifi :: Command -> Wifi [String] [String]
-getKnownWifi (ListKnown cmd) = runWithLog (run cmd) logKnown
+alreadyUsed :: Command -> WifiMonad [String] [SSID]
+alreadyUsed (Scan cmd) = runWithLog (run cmd) logKnown
+  where logKnown = logMsg ("\n Auto-connect wifi: \n") ("- "++)
 
 -- | Runs a computation and logs f on the computation results
-runWithLog :: (Monoid b) => IO a -> (a -> b) -> Wifi b a
+runWithLog :: (Monoid b) => IO a -> (a -> b) -> WifiMonad b a
 runWithLog comp f = do
   result <- liftIO comp
   tell $ f result
   return result
 
--- runWithLog :: (Monoid b) => IO a -> (a -> b) -> Wifi b a
--- runWithLog comp f = liftIO comp >>= (\result -> (tell $ f result) >>  return result)
-
 -- | Elect wifi according to signal's power joined to a list of auto connect ones
-elect :: [String] -> [String] -> String
+elect :: [String] -> [SSID] -> SSID
 elect known = head . intersect known
 
 logMsg :: String -> (String -> String) -> [String] -> [String]
@@ -95,8 +93,8 @@ logMsg prefix f = (prefix :) . map f
 --}
 main :: IO ()
 main = do
-  (allWifis, msg1)   <- runWriterT $ scanWifis scanCmd
-  (knownWifis, msg2) <- runWriterT $ getKnownWifi knownCmd
-  run . connectCmd $ elect knownWifis allWifis
+  (allWifis, msg1)   <- runWriterT $ available scanCmd
+  (knownWifis, msg2) <- runWriterT $ alreadyUsed knownCmd
+  run . connect conCmd $ elect knownWifis allWifis
   -- run . commandConnectToWifi $ elect knownWifis allWifis `catchError` return ["No wifi found"]
   mapM_ putStrLn $ msg1 ++ msg2
