70f23edadee43a430ffe4333b58ee1a626d2e4f6
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-27 19:11:03 +0200

    Refactor - Format code

--- Network/Types.hs
+++ Network/Types.hs
@@ -8,11 +8,14 @@ type Signal= String
 type Wifi  = (SSID, Signal)
 type Log   = String
 
-data Command = Scan{ scan :: String} | Connect {connect :: String -> String}
+data Command = Scan { scan :: String }
+             | Connect { connect :: String -> String }
 
 instance Show Command where
   show (Scan _) = "Scanning for finding some Wifi"
   show (Connect _) = "Connecting to an elected Wifi..."
 
-
-data CommandError = EmptyCommand| InvalidCommand| OtherError String  deriving (Show, Eq)
+data CommandError = EmptyCommand
+                  | InvalidCommand
+                  | OtherError String
+                  deriving (Show, Eq)
