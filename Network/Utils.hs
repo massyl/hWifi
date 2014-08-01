513da88642921220118faed4e22817edea88ace4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-01 12:15:18 +0200

    Fix build ci
    Error message:
      Ambiguous occurrence `catch'
      It could refer to either `Prelude.catch',
      imported from `Prelude' at Network/Utils.hs:1:8-20
      (and originally defined in `System.IO.Error')
      or `Control.Exception.catch',
      imported from `Control.Exception' at Network/Utils.hs:21:27-31
      (and originally defined in `Control.Exception.Base')

--- Network/Utils.hs
+++ Network/Utils.hs
@@ -41,5 +41,5 @@ logMsg prefix f = (prefix :) . map f
 -- | Executes a given `IO a` action, catches and print to stderr any thrown
 -- | exception, then returns a defValue and continues execution
 catchIO :: MonadIO m => IO a -> a -> m a
-catchIO ma defValue = liftIO (ma `catch` \(SomeException e) ->
+catchIO ma defValue = liftIO (ma `Control.Exception.catch` \(SomeException e) ->
                       hPrint stderr e >> hFlush stderr >> return defValue)
