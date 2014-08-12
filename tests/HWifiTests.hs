80d891256c92da59c9bdc56fd6743618ca651190
Antoine R. Dumont <eniotna.t@gmail.com>
2014-08-12 09:08:19 +0200

    Refactor - Simplify test format

--- tests/HWifiTests.hs
+++ tests/HWifiTests.hs
@@ -9,70 +9,64 @@ import Network.Utils ( run
                      , formatMsg
                      , split)
 
-quote::Char
-quote = '\''
 
 testCommandScanWifi, testKnownCommand :: Test.HUnit.Test
-testCommandScanWifi = "nmcli --terse --fields ssid,signal dev wifi" ~=? scan scanCmd
-testKnownCommand = "nmcli --terse --fields name con list"           ~=? scan knownCmd
+testCommandScanWifi = "Nmcli - Scan command"            ~: "nmcli --terse --fields ssid,signal dev wifi" ~=? scan scanCmd
+testKnownCommand    = "Nmcli - List known wifi command" ~: "nmcli --terse --fields name con list"        ~=? scan knownCmd
 
-testCleanString1, testCleanString2, testCleanString3, testCleanString4, testCleanStrings :: Test.HUnit.Test
-testCleanString1 = "hello" ~=? clean quote "'hello'"
-testCleanString2 = "hello" ~=? clean quote "'hello"
-testCleanString3 = "hello" ~=? clean quote "hello"
-testCleanString4 = "hello'" ~=? clean quote "hello'"
-testCleanStrings = TestList ["testCleanString1" ~: testCleanString1
-                             ,"testCleanString2" ~: testCleanString2
-                             ,"testCleanString3" ~: testCleanString3
-                             ,"testCleanString4" ~: testCleanString4]
+testCleanStrings :: Test.HUnit.Test
+testCleanStrings =
+  TestList [ "testCleanString1" ~: "hello" ~=? clean quote "'hello'"
+           , "testCleanString2" ~: "hello" ~=? clean quote "'hello"
+           , "testCleanString3" ~: "hello" ~=? clean quote "hello"
+           , "testCleanString4" ~: "hello'" ~=? clean quote "hello'"
+           ]
+  where quote = '\''
 
-testConnectToWifiCommand1, testConnectToWifiCommand2, testConnectToWifiCommands :: Test.HUnit.Test
-testConnectToWifiCommand1 = "sudo nmcli con up id tatooine" ~=? connect conCmd "tatooine"
-testConnectToWifiCommand2 = "sudo nmcli con up id "         ~=? connect conCmd []
-testConnectToWifiCommands = TestList ["testConnectToWifiCommand1" ~: testConnectToWifiCommand1
-                                     ,"testConnectToWifiCommand2" ~: testConnectToWifiCommand2]
+testConnectToWifiCommands :: Test.HUnit.Test
+testConnectToWifiCommands =
+  TestList [ "Nmcli - test connect to wifi command - with wifi" ~: "sudo nmcli con up id tatooine" ~=? connect conCmd "tatooine"
+           , "Nmcli - test connect to wifi command - empty"     ~: "sudo nmcli con up id "         ~=? connect conCmd []
+           ]
 
 
-testElectWifi1, testElectWifis :: Test.HUnit.Test
-testElectWifi1 = Right "some-wifi-alone" ~=? unsafeElect (Right ["some-wifi-alone", "wifi2", "wifi3"])
-                                                         (Right ["some-wifi-alone","known1", "known2"])
-testElectWifi2 = Left NoWifiAvailable ~=? unsafeElect (Right []) (Right ["some-wifi-alone","known1", "known2"])
-testElectWifi3 = Left NoWifiAvailable ~=? unsafeElect (Right ["wifi0"]) (Right ["some-wifi-alone"])
-testElectWifi4 = Left NoWifiAvailable ~=? unsafeElect (Left NoWifiAvailable) (Right ["some-wifi-alone"])
-testElectWifi5 = Left KnownWifiError ~=? unsafeElect (Right ["wifi0"]) (Left KnownWifiError)
-testElectWifis = TestList ["testElectWifi1" ~: testElectWifi1
-                          ,"testElectWifi2" ~: testElectWifi2
-                          ,"testElectWifi3" ~: testElectWifi3
-                          ,"testElectWifi4" ~: testElectWifi4
-                          ,"testElectWifi5" ~: testElectWifi5
-                          ]
+testElectWifis :: Test.HUnit.Test
+testElectWifis =
+  TestList ["Elect wifi - scanned wifi `intersect` known wifi == 'some-wifi-alone'" ~:
+            Right "some-wifi-alone" ~=? unsafeElect (Right ["some-wifi-alone", "wifi2", "wifi3"]) (Right ["some-wifi-alone","known1", "known2"])
+           ,"Elect wifi - no wifi available - no scanned wifi" ~:
+            Left NoWifiAvailable ~=? unsafeElect (Right []) (Right ["some-wifi-alone","known1", "known2"])
+           ,"Elect wifi - No wifi available - scanned wifi `intersect` known wifi == []" ~:
+            Left NoWifiAvailable ~=? unsafeElect (Right ["wifi0"]) (Right ["some-wifi-alone"])
+           ,"Elect wifi - Error - Error in scanned wifi so error is transmitted" ~:
+            Left NoWifiAvailable ~=? unsafeElect (Left NoWifiAvailable) (Right ["some-wifi-alone"])
+           ,"Elect wifi - Error - Error in known wifi list so error is transmitted  " ~:
+            Left KnownWifiError ~=? unsafeElect (Right ["wifi0"]) (Left KnownWifiError)
+  ]
 
-testRun0, testRun1, testRuns :: Test.HUnit.Test
-testRun0 = TestCase $ run "" >>= assertEqual "Bad command - empty command" (Left $ BadCommand "")
-testRun1 = TestCase $ run "ls -" >>= assertEqual "Bad command - incorrect command" (Left $ BadCommand "ls -")
-testRun2 = TestCase $ run "echo 'tatooine':75\n'myrkr':90" >>= assertEqual "Retrieve output from the command" (Right ["'tatooine':75","'myrkr':90"])
-testRuns = TestList [ "testRun0" ~: testRun0
-                    , "testRun1" ~: testRun1
-                    , "testRun2" ~: testRun2
+testRuns :: Test.HUnit.Test
+testRuns = TestList [ "run - Bad command - Empty command" ~:
+                      TestCase $ run "" >>= assertEqual "Bad command - empty command" (Left $ BadCommand "")
+                    , "run - Bad command - The command is badly formatted, which renders an error" ~:
+                      TestCase $ run "ls -" >>= assertEqual "Bad command - incorrect command" (Left $ BadCommand "ls -")
+                    , "run - Ok - retrieve the command's output result as a [String]" ~:
+                      TestCase $ run "echo 'tatooine':75\n'myrkr':90" >>= assertEqual "Retrieve output from the command" (Right ["'tatooine':75","'myrkr':90"])
                     ]
 
-testFormatMsg0, testFormatMsgs :: Test.HUnit.Test
-testFormatMsg0 = ["No known wifi available!"] ~=? formatMsg "" id (Left NoWifiAvailable)
-testFormatMsg1 = [ "prefix string: "
-                 , "- input 0"
-                 , "- input 1"] ~=? formatMsg "prefix string: " ("- " ++) (Right ["input 0", "input 1"])
-testFormatMsgs = TestList [ "testFormatMsg0" ~: testFormatMsg0
-                          , "testFormatMsg1" ~: testFormatMsg1
-                          ]
+testFormatMsgs :: Test.HUnit.Test
+testFormatMsgs =
+  TestList [ "testFormatMsg0" ~: ["No known wifi available!"] ~=? formatMsg "" id (Left NoWifiAvailable)
+           , "testFormatMsg1" ~: [ "prefix string: "
+                                 , "- input 0"
+                                 , "- input 1"] ~=? formatMsg "prefix string: " ("- " ++) (Right ["input 0", "input 1"])
+           ]
 
-testSplit0, testSplit1, testSplit2, testSplits :: Test.HUnit.Test
-testSplit0 = split "\r\n" "a\r\nb\r\nd\r\ne" ~=? ["a","b","d","e"]
-testSplit1 = split "aaa"  "aaaXaaaXaaaXaaa"  ~=? ["","X","X","X",""]
-testSplit2 = split "x"    "x"                ~=? ["",""]
-
-testSplits = TestList [ "testSplit0" ~: testSplit0
-                      , "testSplit1" ~: testSplit1
-                      , "testSplit2" ~: testSplit2]
+testSplits :: Test.HUnit.Test
+testSplits =
+  TestList [ "testSplit0" ~: split "\r\n" "a\r\nb\r\nd\r\ne" ~=? ["a","b","d","e"]
+           , "testSplit1" ~: split "aaa"  "aaaXaaaXaaaXaaa"  ~=? ["","X","X","X",""]
+           , "testSplit2" ~: split "x"    "x"                ~=? ["",""]
+           ]
 
 -- Not working yet!
 -- testAvailable0, testAvailables :: Test.HUnit.Test
