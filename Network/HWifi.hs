bc4a3f83e4da27a588a758f2d81fd3fdb0f25d59
Antoine R. Dumont <eniotna.t@gmail.com>
2014-08-12 19:09:18 +0200

    Refactor - Simplify using second

--- Network/HWifi.hs
+++ Network/HWifi.hs
@@ -46,7 +46,7 @@ available (Scan cmd)  = runWithLog wifis logMsg
                                 Right ssids -> Right $ (map fst . sortBy (flip compare `on` snd) . map parse) ssids
                                            where -- | Slice a string "'wifi':signal" in a tuple ("wifi", signal)
                                                  parse :: Output -> Wifi
-                                                 parse = (id *** \y -> read y :: Integer) . (clean '\'' *** tail) . break (== ':')
+                                                 parse = second (\y -> read y :: Integer) . (clean '\'' *** tail) . break (== ':')
                               wifis :: IO (ThrowsError [SSID])
                               wifis = parseOutput <$> run cmd `catchIO` Left ScanWifiError
                               logMsg :: ThrowsError [SSID] -> [Log]
