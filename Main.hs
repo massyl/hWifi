d361cd9035724ecc80688275cc455f6f137a6d6e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-19 13:44:50 +0200

    Update hWifi CLI launcher
    If bad input from the user (missing options for example), this stops the
    program and display how to use it.

--- Main.hs
+++ Main.hs
@@ -15,12 +15,18 @@ module Main (main
 -- Portability :  unportable
 -- Dependency  :  nmcli (network-manager package in debian-based platform - http://www.gnome.org/projects/NetworkManager/) + checkbox (System testing application)
 --
--- A module to deal with wifi connections.
--- 2 use cases:
--- - Determine the most powerful wifi signal amongst known auto-connect wifis and connect to it.
--- - If provided with (ssid, wifiSecurity {wpa or wep}, psk) in this order, this will create a new auto-connect entry and connect to it.
+-- A module to deal with wifi connections, 2 main use cases:
+-- - Automatic: Determine the most powerful wifi signal amongst known auto-connect wifis and connect to it.
+-- - Manual: If provided with (ssid, connection policy {wpa or wep}, and pre-shared key), this will create a new auto-connect entry and connect to it.
+
+-- Usage: hwifi [OPTION...] files...
+--  -V, -?        --version                   Show version number
+--  -a            --auto                      Standard auto-connect policy. This is the default behavior
+--  -s SSID       --ssid=SSID                 wifi SSID to connect to.
+--  -c <wpa|wep>  --connect-policy=<wpa|wep>  The connection policy (wep or wpa)
+--  -p <psk>      --psk=<psk>                 Pre-Shared Key to connect to the ssid.
 --
--- Use:
+-- For example:
 -- - `cabal run` for standard auto-connect policy to known wifi
 -- - `cabal run <ssid> <wifiSecurity> <psk>` to create a new auto-connect wifi entry
 --
@@ -33,8 +39,7 @@ import           Network.StandardPolicy (alreadyUsedWifis, availableWifis,
 import           System.Console.GetOpt
 import           System.Environment
 
-data Options = Options { optVerbose       :: Bool
-                       , optShowVersion   :: Bool
+data Options = Options { optShowVersion   :: Bool
                        , optAuto          :: Bool
                        , optSSID          :: Maybe String
                        , optConnectPolicy :: Maybe String
@@ -42,8 +47,7 @@ data Options = Options { optVerbose       :: Bool
                        } deriving Show
 
 defaultOptions :: Options
-defaultOptions    = Options { optVerbose       = False
-                            , optShowVersion   = False
+defaultOptions    = Options { optShowVersion   = False
                             , optAuto          = True
                             , optSSID          = Nothing
                             , optConnectPolicy = Nothing
@@ -52,32 +56,36 @@ defaultOptions    = Options { optVerbose       = False
 
 options :: [OptDescr (Options -> Options)]
 options =
-  [ Option "v" ["verbose"]        (NoArg (\ opts -> opts { optVerbose = True }))                        "Chatty output on stderr"
-  , Option "V?"["version"]        (NoArg (\ opts -> opts { optShowVersion = True }))                    "Show version number"
+  [ Option "V?"["version"]        (NoArg (\ opts -> opts { optShowVersion = True }))                    "Show version number"
   , Option "a" ["auto"]           (NoArg (\ opts -> opts { optAuto = True }))                           "Standard auto-connect policy. This is the default behavior"
   , Option "s" ["ssid"]           (ReqArg (\ f opts -> opts { optSSID = Just f }) "SSID")               "wifi SSID to connect to."
   , Option "c" ["connect-policy"] (ReqArg (\ f opts -> opts { optConnectPolicy = Just f }) "<wpa|wep>") "The connection policy (wep or wpa)"
   , Option "p" ["psk"]            (ReqArg (\ f opts -> opts { optPsk = Just f }) "<psk>")               "Pre-Shared Key to connect to the ssid."
   ]
 
+usage :: [String] -> IO a
+usage errs = ioError (userError (concat errs ++ usageInfo header options))
+             where header = "\nUsage: hwifi [OPTION...] files..."
+
 compilerOpts :: [String] -> IO Options
 compilerOpts argv =
   case getOpt Permute options argv of
      (opts,_,[]) -> return $ foldl (flip id) defaultOptions opts
-     (_,_,errs)  -> ioError (userError (concat errs ++ usageInfo header options))
-  where header = "Usage: hwifi [OPTION...] files..."
+     (_,_,errs)  -> usage errs
+
+version :: String
+version = "0.0.0.1"
+
+eval :: Options -> IO ()
+eval (Options { optShowVersion = True }) = putStrLn $ "hWifi" ++ version
+eval (Options { optAuto = True
+              , optSSID = Nothing
+              , optConnectPolicy = Nothing
+              , optPsk = Nothing })      = scanAndConnectToKnownWifiWithMostPowerfulSignal scanCmd knownCmd conCmd
+eval (Options { optSSID = (Just ssid)
+              , optConnectPolicy = (Just connectPolicy)
+              , optPsk = (Just psk) })   = createNewWifiConnectionAndConnect createCmd ssid connectPolicy psk
+eval _                                   = usage ["Unknown options - Either use automatic connection (-a) or provide ssid (-s) with connect-policy (-c) and psk (-p)."]
 
 main :: IO ()
-main =
-  getArgs >>=
-  compilerOpts >>=
-  \ (Options { optVerbose = verboseFlag
-             , optShowVersion = versionFlag
-             , optAuto = autoFlag
-             , optSSID = ssidOpt
-             , optConnectPolicy = connectPolicyOpt
-             , optPsk = pskOpt }) ->
-               if autoFlag
-                  then scanAndConnectToKnownWifiWithMostPowerfulSignal scanCmd knownCmd conCmd
-                  else let (Just ssid, Just connectPolicy, Just psk) = (ssidOpt, connectPolicyOpt, pskOpt)
-                       in createNewWifiConnectionAndConnect createCmd ssid connectPolicy psk
+main = getArgs >>= compilerOpts >>= eval
