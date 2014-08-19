65cf6e26629d760aa7f7660bd70d521828e148db
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-19 13:00:25 +0200

    Add temporary parsing solution - not satisfied by the usage

--- Main.hs
+++ Main.hs
@@ -30,15 +30,46 @@ import           Network.Nmcli          (conCmd, createCmd, knownCmd, scanCmd)
 import           Network.StandardPolicy (alreadyUsedWifis, availableWifis,
                                          createNewWifiConnectionAndConnect,
                                          electedWifi, scanAndConnectToKnownWifiWithMostPowerfulSignal)
+import           System.Console.GetOpt
 import           System.Environment
+
+data Flag = Verbose
+          | Version
+          | Auto
+          | PSK String
+          | SSID String
+          | ConnectPolicy String
+          | LibDir String
+          deriving Show
+
+options :: [OptDescr Flag]
+options = [ Option "v"  ["verbose"]        (NoArg Verbose)                         "Chatty output on stderr"
+          , Option "V?" ["version"]        (NoArg Version)                         "Show version number"
+          , Option "a"  ["auto"]           (NoArg Auto)                            "Auto-connect policy"
+          , Option "s"  ["ssid"]           (ReqArg SSID "SSID")                    "SSID to connect to"
+          , Option "c"  ["connect-policy"] (ReqArg ConnectPolicy "connect-policy") "wifi connect policy: wpa or wep"
+          , Option "p"  ["psk"]            (ReqArg PSK "pre-shared-key")           "Pre-shared key to connect to the wifi"
+          ]
+
+compilerOpts :: [String] -> IO ([Flag], [String])
+compilerOpts argv =
+   case getOpt Permute options argv of
+      (opts, nonOpts,[]) -> return (opts, nonOpts)
+      (_,_,errs)         -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "Usage: ic [OPTION...] files..."
 
 -- | Main orchestrator
 -- Without argument: Determine the highest known wifi signal and connect to it
 -- With 3 arguments (ssid, wifiSecurity, psk) in this order, create a new wifi session and connect to it
+-- main :: IO ()
+-- main = do
+--   args <- getArgs
+--   let (argv, _) = compilerOpts args in
+--   case any (== Auto) argv of
+--     True -> scanAndConnectToKnownWifiWithMostPowerfulSignal scanCmd knownCmd conCmd
+--     _     -> let (ssid:wifiSecurity:psk:_) = args in
+--              createNewWifiConnectionAndConnect createCmd ssid wifiSecurity psk
+
 main :: IO ()
-main = do
-  args <- getArgs
-  if null args
-     then scanAndConnectToKnownWifiWithMostPowerfulSignal scanCmd knownCmd conCmd
-     else let (ssid:wifiSecurity:psk:_) = args in
-          createNewWifiConnectionAndConnect createCmd ssid wifiSecurity psk
+main =
+  getArgs >>=  compilerOpts >>= \flags -> mapM_ print $ fst flags
