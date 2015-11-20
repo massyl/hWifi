267c4560c0535ba8c83720d4a0b650c6590a7aed
Antoine R. Dumont <eniotna.t@gmail.com>
2015-11-20 16:24:42 +0100

    Update nix targets to init the project

--- hwifi.nix
+++ hwifi.nix
@@ -1,25 +1,32 @@
-{ pkgs ? (import <nixpkgs> {})
-, haskellPackages ? pkgs.haskellPackages_ghc784_no_profiling
-, networkmanager ? pkgs.networkmanager # nmcli used by hWifi
-}:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 
 let
-  inherit (haskellPackages) cabal cabalInstall HUnit QuickCheck mtl text; # Haskell dependencies here
 
-in cabal.mkDerivation (self: {
-  pname = "hWifi";
-  version = "0.0.0.1";
-  sha256 = "dummy-sha1-for-the-moment";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  buildTools = [ haskellPackages.cabalInstall networkmanager ];
-  buildDepends = with haskellPackages; [ mtl text ];
-  testDepends = with haskellPackages; [ HUnit QuickCheck text ];
-  meta = {
-    homepage = "https://github.com/lambdatree/hWifi.git";
-    description = "Wifi connections manager in Haskell";
-    license = self.stdenv.lib.licenses.gpl2;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, HUnit, mtl, process, QuickCheck, stdenv
+      , text
+      }:
+      mkDerivation {
+        pname = "hWifi";
+        version = "0.0.0.1";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base mtl process text ];
+        executableHaskellDepends = [ base mtl process text ];
+        testHaskellDepends = [ base HUnit QuickCheck text ];
+        homepage = "https://github.com/lambdatree/hWifi.git";
+        description = "Wifi connections manager in Haskell";
+        license = stdenv.lib.licenses.gpl2;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
